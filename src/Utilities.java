e9ae1903cc503468dc40b2fac95c5696df4e1084
Santokh Singh Dhillon <santokhsdg@gmail.com>
2017-06-07 15:45:18 +0530

    negative, greyscale add to utilities

--- src/Utilities.java
+++ src/Utilities.java
@@ -22,5 +22,62 @@ public class Utilities {
        Image scimage = img.getScaledInstance(rect.width,rect.height,Image.SCALE_DEFAULT);
        return new ImageIcon(scimage);
     }
+    
+    public static void convertToGreyscale(BufferedImage img){
+        //get image width and height
+    int width = img.getWidth();
+    int height = img.getHeight();
 
-}
+    //convert to grayscale
+    for(int y = 0; y < height; y++){
+      for(int x = 0; x < width; x++){
+        int p = img.getRGB(x,y);
+
+        int a = (p>>24)&0xff;
+        int r = (p>>16)&0xff;
+        int g = (p>>8)&0xff;
+        int b = p&0xff;
+
+        //calculate average
+        int avg = (r+g+b)/3;
+
+        //replace RGB value with avg
+        p = (a<<24) | (avg<<16) | (avg<<8) | avg;
+
+        img.setRGB(x, y, p);
+      }
+    }
+    }
+    public static void convertToNegative(BufferedImage img){
+        //get image width and height
+    int width = img.getWidth();
+    int height = img.getHeight();
+
+    //convert to grayscale
+    for(int y = 0; y < height; y++){
+      for(int x = 0; x < width; x++){
+        int p = img.getRGB(x,y);
+
+        int a = (p>>24)&0xff;
+        int r = (p>>16)&0xff;
+        int g = (p>>8)&0xff;
+        int b = p&0xff;
+        
+        r=255-r;
+        g=255-g;
+        b=255-b;
+        //calculate average
+        int avg = (r+g+b)/3;
+
+        //replace RGB value with avg
+        p = (a<<24) | (avg<<16) | (avg<<8) | avg;
+
+        img.setRGB(x, y, p);
+      }
+    }
+    }
+      
+    
+    }
+
+
